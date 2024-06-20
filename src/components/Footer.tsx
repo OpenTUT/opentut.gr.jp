48081d3026dfd1b08a38a1a8d206557d3b0a6060
Rin Iwai <rin@eyrin.jp>
2024-06-20 22:47:16 +0900

    delete footer

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { SITE_TITLE } from '../consts';
-
-export function Footer() {
-  return (
-    <footer className="bg-gray-800 p-4 text-center text-white">
-      <p>&copy; 2024 {SITE_TITLE}</p>
-    </footer>
-  );
-}
