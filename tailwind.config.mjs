722629aab7eef3258ae5dc26ec86744acfd585eb
Rin Iwai <rin@eyrin.jp>
2024-06-20 18:33:00 +0900

    typography

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import typography from '@tailwindcss/typography';
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
@@ -7,6 +10,14 @@ export default {
         'tut-red': '#c7000a',
       },
     },
+    typography: {
+      DEFAULT: {
+        css: {
+          'blockquote p:first-of-type::before': false,
+          'blockquote p:first-of-type::after': false,
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [typography],
 };
