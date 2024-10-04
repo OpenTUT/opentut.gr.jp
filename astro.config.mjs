df6eecf2d1a334f4d0a31f00695830739b99defd
Rin Iwai <rin@eyrin.jp>
2024-10-04 15:33:39 +0900

    katex導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import behead from 'remark-behead';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://opentut.gr.jp',
   integrations: [react(), tailwind()],
   markdown: {
-    remarkPlugins: [[behead, { minDepth: 2 }]],
+    remarkPlugins: [[behead, { minDepth: 2 }], remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
 });
